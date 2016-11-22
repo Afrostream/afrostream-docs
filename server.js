325e0226c0475d31f8f5c9524864f2230e3b6527
nelsounet <nelsounet@afrostream.tv>
2016-11-22 16:24:15 +0100

    added first billing api doc

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use('/api', express.static(__dirname + '/api'));
+app.use(‘/billings’, express.static(__dirname + ‘/billings’));
 
 app.listen(process.env.PORT || 13333, function () {
   console.log('up');
